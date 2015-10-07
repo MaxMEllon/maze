727e8ff865944a35ee6a1148962890d4ff6da04e
MaxMEllon <mozi_kke@excite.co.jp>
2015-10-07 16:04:54 +0900

    :red_car: Change: Window class to struct
    * private なメンバを持たないので構造体へ変更

--- src/include/window.h
+++ src/include/window.h
@@ -3,17 +3,16 @@
 
 #include "env.h"
 
-class Window
+struct Window
 {
-  public:
-    static bool enable_color;
-    static int  getKeyStroke();
-    static void addString(const std::string string);
-    static void eraseBuff();
-    static void moveCursol(int x, int y);
-    static void refreshWindow();
-    static void initalize();
-    static void free();
+  static bool enable_color;
+  static int  getKeyStroke();
+  static void addString(const std::string string);
+  static void eraseBuff();
+  static void moveCursol(int x, int y);
+  static void refreshWindow();
+  static void initalize();
+  static void free();
 };
 
 #endif
